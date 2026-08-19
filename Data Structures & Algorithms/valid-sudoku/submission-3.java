a62cb814bf5ca563c4f14638ee0b67082958aaae
Arpit Saxena <saxenaarpit61@gmail.com>
2026-08-19 10:28:23 +0530

    Add: valid-sudoku - submission-3

---
+++ Data Structures & Algorithms/valid-sudoku/submission-3.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        Set<String> seen = new HashSet<>();
+        for(int i=0 ; i<9 ; i++)
+        {
+            for(int j=0 ; j<9 ; j++)
+            {
+                int val = board[i][j];
+                if(val!='.'){
+                    if(!seen.add(val+ "in row" + i) || !seen.add(val+"in col"+j) || !seen.add(val+ "in box" + i/3 + "-" + j/3))
+                    return false;
+                }
+            }
+        }
+    return true;
+    }
+}
